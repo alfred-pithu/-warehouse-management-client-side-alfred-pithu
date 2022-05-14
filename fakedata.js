1a640d5509c367ed4589a55e3e0e3f9131e1dca0
unknown <alfredpithu@gmail.com>
2022-05-15 00:23:16 +0600

    quantitiy update done

--- fakedata.js
+++ fakedata.js
@@ -24,3 +24,62 @@ const data = [
     { name: 'Airtag', img: 'https://i.ibb.co/rGxBM62/airtag.png', description: 'AirTag is an easy way to keep track of your stuff. Attach one to your keys, slip another one in your backpack.', price: '30$', quantity: '50', sold: '20', supplier: 'Apple Inc' }
 
 ]
+
+
+
+
+// ---------------------------------------
+
+const kj = [
+
+    { "name": "Ipad", "img": "https://i.ibb.co/JF4z0DH/ipad.png", "description": "The 10.9-inch Liquid Retina display features advanced technologies like True Tone, P3 wide color, and an antireflective coating.", "price": "500$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Imac", "img": "https://i.ibb.co/JB9f4xQ/imac.png", "description": "The 24-inch iMac. 7 colors. Impossibly thin. 4.5K Retina display. The best camera, mics, and speakers. Supercharged by the M1 chip.", "price": "2000$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Macbook Air", "img": "https://i.ibb.co/ykVzJtQ/macbookairagain.jpg", "description": "Powerfully portable. The incredible Apple M1 chip. Fast performance. All-day battery life. Touch ID. And brilliant Retina display.", "price": "1500$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Apple TV", "img": "https://i.ibb.co/KzBtkz7/apple-tv11.png", "description": "Simply connect Apple TV, HomePod mini, and other accessories to experience a smarter, more convenient home that runs across your devices.", "price": "1800$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Apple Watch", "img": "https://i.ibb.co/MPqt41w/smartwatch-blue-1.jpg", "description": "Apple Watch is the ultimate device for a healthy life. Choose from a variety of models including the all-new Apple Watch Series 7.", "price": "400$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Ipod", "img": "https://i.ibb.co/SVWm581/ipod.png", "description": "Now with the A10 Fusion chip, which powers augmented reality games and apps. Group FaceTime. And up to 256GB of storage.", "price": "200$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "XDR Display", "img": "https://i.ibb.co/J2s7Cdz/xdr-display.png", "description": "Pro Display XDR. A 32-inch Retina 6K display with extreme brightness, contrast, color, and HDR that transforms the way you work.", "price": "1300$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Mac Mini", "img": "https://i.ibb.co/d2R0nnf/macmini111.png", "description": "The incredible Apple M1 chip, up to 3x faster CPU performance, up to 6x faster graphics, and all-flash SSD storage up to 2TB.", "price": "2500$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Iphone 13", "img": "https://i.ibb.co/LR5THvq/iphone13converted.jpg", "description": "iPhone 13 Pro and 13 Pro Max. Huge camera upgrades. All-screen OLED display with ProMotion. Fastest iPhone chip ever. Breakthrough battery life.", "price": "1500$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Airpods", "img": "https://i.ibb.co/0Qq33YN/airpods-white.png", "description": "AirPods Pro have been designed to deliver Active Noise Cancellation for immersive sound, Transparency mode so you can hear your surroundings.", "price": "250$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Dongle", "img": "https://i.ibb.co/KbxQMvR/dongle.jpg", "description": "Works with all devices that have a Lightning connector and support iOS 10 or later, including iPod touch, iPad, and iPhone.", "price": "30$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" },
+
+    { "name": "Airtag", "img": "https://i.ibb.co/rGxBM62/airtag.png", "description": "AirTag is an easy way to keep track of your stuff. Attach one to your keys, slip another one in your backpack.", "price": "30$", "quantity": 50, "sold": 20, "supplier": "Apple Inc" }
+]
+
+
+const jj = [
+    { "name": "Ipad", "img": "https://i.ibb.co/JF4z0DH/ipad.png", "description": "The 10.9-inch Liquid Retina display features advanced technologies like True Tone, P3 wide color, and an antireflective coating.", "price": "500$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Imac", "img": "https://i.ibb.co/JB9f4xQ/imac.png", "description": "The 24-inch iMac. 7 colors. Impossibly thin. 4.5K Retina display. The best camera, mics, and speakers. Supercharged by the M1 chip.", "price": "2000$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Macbook Air", "img": "https://i.ibb.co/ykVzJtQ/macbookairagain.jpg", "description": "Powerfully portable. The incredible Apple M1 chip. Fast performance. All-day battery life. Touch ID. And brilliant Retina display.", "price": "1500$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Apple TV", "img": "https://i.ibb.co/KzBtkz7/apple-tv11.png", "description": "Simply connect Apple TV, HomePod mini, and other accessories to experience a smarter, more convenient home that runs across your devices.", "price": "1800$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Apple Watch", "img": "https://i.ibb.co/MPqt41w/smartwatch-blue-1.jpg", "description": "Apple Watch is the ultimate device for a healthy life. Choose from a variety of models including the all-new Apple Watch Series 7.", "price": "400$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Ipod", "img": "https://i.ibb.co/SVWm581/ipod.png", "description": "Now with the A10 Fusion chip, which powers augmented reality games and apps. Group FaceTime. And up to 256GB of storage.", "price": "200$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "XDR Display", "img": "https://i.ibb.co/J2s7Cdz/xdr-display.png", "description": "Pro Display XDR. A 32-inch Retina 6K display with extreme brightness, contrast, color, and HDR that transforms the way you work.", "price": "1300$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Mac Mini", "img": "https://i.ibb.co/d2R0nnf/macmini111.png", "description": "The incredible Apple M1 chip, up to 3x faster CPU performance, up to 6x faster graphics, and all-flash SSD storage up to 2TB.", "price": "2500$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Iphone 13", "img": "https://i.ibb.co/LR5THvq/iphone13converted.jpg", "description": "iPhone 13 Pro and 13 Pro Max. Huge camera upgrades. All-screen OLED display with ProMotion. Fastest iPhone chip ever. Breakthrough battery life.", "price": "1500$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Airpods", "img": "https://i.ibb.co/0Qq33YN/airpods-white.png", "description": "AirPods Pro have been designed to deliver Active Noise Cancellation for immersive sound, Transparency mode so you can hear your surroundings.", "price": "250$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Dongle", "img": "https://i.ibb.co/KbxQMvR/dongle.jpg", "description": "Works with all devices that have a Lightning connector and support iOS 10 or later, including iPod touch, iPad, and iPhone.", "price": "30$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" },
+
+    { "name": "Airtag", "img": "https://i.ibb.co/rGxBM62/airtag.png", "description": "AirTag is an easy way to keep track of your stuff. Attach one to your keys, slip another one in your backpack.", "price": "30$", "quantity": "50", "sold": "20", "supplier": "Apple Inc" }
+]
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -7,39 +7,48 @@ const Inventory = () => {
     const [productData, setProductData] = useState({})
     const { _id, name, img, description, price, supplier, quantity, sold } = productData;
 
+
     useEffect(() => {
         fetch(`http://localhost:5000/singleItem/${id}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify()
         })
             .then(res => res.json())
             .then(data => setProductData(data))
 
-    }, [id])
-    console.log(productData);
+    }, [])
+
+    // console.log(productData);
+
+    const delivered = () => {
+        fetch(`http://localhost:5000/decreasequantity/${_id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify()
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+    };
+
     return (
         <div className='container'>
             <h2>Inventory: {id} </h2>
 
-            <div class="card mb-3" >
-                <div class="row g-0">
-                    <div class="col-md-4 border d-flex align-items-center p-3">
-                        <img className='' src={img} class="img-fluid rounded-start" alt="..." />
+            <div className="card mb-3" >
+                <div className="row g-0">
+                    <div className="col-md-4 border d-flex align-items-center p-3">
+                        <img src={img} className="img-fluid rounded-start" alt="..." />
                     </div>
-                    <div class="col-md-8 d-flex align-items-center">
-                        <div class="card-body">
-                            <h5 class="card-title">{name}</h5>
+                    <div className="col-md-8 d-flex align-items-center">
+                        <div className="card-body">
+                            <h5 className="card-title">{name}</h5>
                             <br />
-                            <p class="card-text">{description}</p>
+                            <p className="card-text">{description}</p>
                             <h6>Price: {price}</h6>
                             <h6>Supplier: {supplier}</h6>
                             <h6>Quantity: {quantity}</h6>
                             <h6>Sold: {sold}</h6>
                             <br />
-                            <button className='btn btn-primary'>Delivered</button>
+                            <button onClick={delivered} className='btn btn-primary'>Delivered</button>
                         </div>
                     </div>
                 </div>
