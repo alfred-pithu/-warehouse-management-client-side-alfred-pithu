11e54f4fb075cb982c963842fd858e61277dcf2e
unknown <alfredpithu@gmail.com>
2022-05-16 02:40:44 +0600

    minor

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,16 +6,23 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDERID,
+//     appId: process.env.APP_ID
+// };
+
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDERID,
-    appId: process.env.APP_ID
+    apiKey: "AIzaSyC1TwZ2PbT_T12mmBREZnxXIUi6GlVSYFw",
+    authDomain: "assignment-11-67ccf.firebaseapp.com",
+    projectId: "assignment-11-67ccf",
+    storageBucket: "assignment-11-67ccf.appspot.com",
+    messagingSenderId: "953134081260",
+    appId: "1:953134081260:web:f86575f2e1f410d4ae71c9"
 };
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
